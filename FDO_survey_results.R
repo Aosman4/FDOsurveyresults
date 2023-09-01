a9137189027efc3a1fb0957083790df564f99d1d
Aosman4 <142908891+Aosman4@users.noreply.github.com>
2023-09-01 09:59:24 -0400

    Update FDO_survey_results.R
    reminder to add the token information

--- FDO_survey_results.R
+++ FDO_survey_results.R
@@ -3,9 +3,7 @@ library(shinydashboard)
 library(ggplot2)
 library(rsconnect)
 
-rsconnect::setAccountInfo(name='aosman4',
-                          token='0C55A46CD316C63C6E89E436DFACBBBD',
-                          secret='AZhZnE6NC2TUCxesP8h+LYYxRT3ljWOcdSmTIObm')
+rsconnect::setAccountInfo
 
 data <-read.csv("https://raw.githubusercontent.com/Aosman4/FDOsurveyresults/main/FDOresultsdata.csv")
 ui = 
